915ccd785e16ca56930a28745f952efc8e29c0dc
Mikael Hansson <drhteph@gmail.com>
2017-07-13 09:49:45 +0200

    Meddling with fOOD to get it verbose and different between ticket classes

--- MiJoPaBooking/src/FOOD.java
+++ MiJoPaBooking/src/FOOD.java
@@ -1,5 +1,21 @@
 
 public enum FOOD {
-	NOTHING, VEGETARIAN, FISH_ALLERGY, LACTOS_INTOLERANT, NOPORK, NOCOW
+	NOTHING ("No meal"), 
+	VEGETARIAN ("Vegetarian" ), 
+	FISH_ALLERGY ("No Fishy stuff"), 
+	LACTOS_INTOLERANT ("No milk products"), 
+	NOPORK ("Halal food"), 
+	NOCOW("Indian cuisine"),
+	POSH(" Campaign and Caviar");
+	
+	private String verboseFood;
+	
+	FOOD(String verboseFood){
+		this.verboseFood = verboseFood;
+	}
+
+	public String getFood() {
+		return verboseFood;
+	}
 
 }
