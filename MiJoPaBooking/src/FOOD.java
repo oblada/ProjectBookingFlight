3f4b77e7cf1f00e255b282de2ace5fc4d2f4a4fd
Mikael Hansson <drhteph@gmail.com>
2017-07-13 09:53:03 +0200

    Added cost to FOOD

--- MiJoPaBooking/src/FOOD.java
+++ MiJoPaBooking/src/FOOD.java
@@ -1,21 +1,27 @@
 
 public enum FOOD {
-	NOTHING ("No meal"), 
-	VEGETARIAN ("Vegetarian" ), 
-	FISH_ALLERGY ("No Fishy stuff"), 
-	LACTOS_INTOLERANT ("No milk products"), 
-	NOPORK ("Halal food"), 
-	NOCOW("Indian cuisine"),
-	POSH(" Campaign and Caviar");
+	NOTHING ("No meal", 0), 
+	VEGETARIAN ("Vegetarian", 120 ), 
+	FISH_ALLERGY ("No Fishy stuff", 120), 
+	LACTOS_INTOLERANT ("No milk products", 120), 
+	NOPORK ("Halal food", 120), 
+	NOCOW("Indian cuisine", 120),
+	POSH(" Campaign and Caviar", 500);
 	
 	private String verboseFood;
+	private int cost;
 	
-	FOOD(String verboseFood){
+	FOOD(String verboseFood, int cost){
 		this.verboseFood = verboseFood;
+		this.cost = cost;
 	}
 
 	public String getFood() {
 		return verboseFood;
+	}
+	
+	public int getCost() {
+		return cost;
 	}
 
 }
