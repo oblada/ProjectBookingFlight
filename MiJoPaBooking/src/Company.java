31bb6e567280dcfd5fa698d5e6c64ba8bba45311
youreakim <youreakim@youreakim-pc>
2017-07-13 12:10:42 +0200

    latest  pasengerplane

--- MiJoPaBooking/src/Company.java
+++ MiJoPaBooking/src/Company.java
@@ -3,12 +3,12 @@ import java.util.ArrayList;
 public class Company {
 	
 	private ArrayList<AirPlane> airplanes	= new ArrayList<AirPlane>();
-	private ArrayList<Flight> flights		= new ArrayList<Flight>;
+	private ArrayList<Flight> flights		= new ArrayList<Flight>();
 	
 	public Company() {
-//		airplanes.add(new PassengerPlane(airPlaneName, numberOfSeatsFirstClass, numberOfSeatsBusinessClass, flyHours, i, modelName, model));
-//		airplanes.add(new PassengerPlane(airPlaneName, numberOfSeatsFirstClass, numberOfSeatsBusinessClass, flyHours, i, modelName, model));
-//		airplanes.add(new PassengerPlane(airPlaneName, numberOfSeatsFirstClass, numberOfSeatsBusinessClass, flyHours, i, modelName, model));
+		airplanes.add(new PassengerPlane("CrashN", 5, 5, "Tupol", "V"));
+		airplanes.add(new PassengerPlane("Burn", 10, 20, "Sputter", "000"));
+		airplanes.add(new PassengerPlane("N2DGround", 8, 14, "Sparkles", "Hindenburg"));
 		
 	}
 	public ArrayList<AirPlane> getAirplanes() {
@@ -24,11 +24,38 @@ public class Company {
 	}
 	
 	public void addFlight(Flight flight) {
-		flights.add(flight)
+		flights.add(flight);
+	}
+	
+	public Flight findFlight(int flightNumber) {
+		for (Flight flight : flights) {
+			if (flight.getFlightSerialNumber() == flightNumber) {
+				return flight;
+			}
+		}
+		return null;
 	}
 	
 	public void calcuteFlightResult(int flightNumber) {
-		Flight flight						= 
+		Flight flight						= findFlight(flightNumber);
+		
+		if (flight != null) {
+			int totalIncome						= 0;
+			
+			for (Ticket ticket : flight.getTicket()) {
+				totalIncome += ticket.getTicketName().getCostOfTicket();
+			}
+			
+			int totalCost	= TICKETTYPE.ECONOMY.getCostOfTicket() * flight.getAirPlane().numberOfSeatsBusinessClass;
+			totalCost += TICKETTYPE.FIRST.getCostOfTicket() * flight.getAirPlane().numberOfSeatsFirstClass;
+			totalCost *= 0.7;
+			
+			if (totalCost > totalIncome) {
+				System.out.println("The company made a loss of " + (totalCost - totalIncome));
+			} else {
+				System.out.println("The company made a profit of " + (totalIncome - totalCost));
+			}
+		}
 	}
 	
 }
--- MiJoPaBooking/src/PassengerPlane.java
+++ MiJoPaBooking/src/PassengerPlane.java
@@ -1,5 +1,6 @@
-import java.util.Date;
 
+
+//Latest
 public class PassengerPlane extends AirPlane {
 	
 	private String modelName;
